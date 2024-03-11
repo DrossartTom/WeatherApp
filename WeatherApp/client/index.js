1905dc26a2bb4228f37aac742a2b716c72d67a43
Tom Drossart <r0999139@ucll.be>
2024-03-11 09:05:58 +0100

    first commit

---
+++ WeatherApp/client/index.js
@@ -0,0 +1,26 @@
+const key = "9f2983fdb06848047c39ea639ad811cc";
+const form = document.querySelector("form");
+const stadInput = document.getElementById("Stad");
+const tbody = document.querySelector("tbody")
+
+form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+
+    let ingevuldeStad = stadInput.value;
+
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${ingevuldeStad}&appid=${key}`;
+    let response = await fetch(url);
+    if (response.ok) {
+        let weerberichtObject = await response.json();
+        weerberichtObject.list.forEach(weerVoor3uur => {
+            console.log(weerVoor3uur)
+            tbody.innerHTML += `
+            <tr>
+                <td>${weerVoor3uur.dt_txt}</td>
+                <td>${(weerVoor3uur.main.temp - 273.15).toFixed(1)}</td>
+                <td>${weerVoor3uur.weather[0].description}</td>
+            </tr>`;
+        });
+    }
+
+});
