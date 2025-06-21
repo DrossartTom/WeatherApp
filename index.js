bca0a9cf629d3155b633f638e94b21b91c8c3feb
DrossartTom <r0999139@ucll.be>
2025-06-21 20:44:24 +0200

    translated to english

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const key = "9f2983fdb06848047c39ea639ad811cc";
 const form = document.querySelector("form");
-const stadInput = document.getElementById("Stad");
-const tbody = document.querySelector("tbody")
+const cityInput = document.getElementById("City");
+const tbody = document.querySelector("tbody");
 
 form.addEventListener('submit', async (evt) => {
     evt.preventDefault();
 
-    let ingevuldeStad = stadInput.value;
+    let enteredCity = cityInput.value.trim();
 
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${ingevuldeStad}&appid=${key}`;
-    let response = await fetch(url);
-    if (response.ok) {
-        tbody.innerHTML = ""
-        let weerberichtObject = await response.json();
-        weerberichtObject.list.forEach(weerVoor3uur => {
-
-            tbody.innerHTML += `
-            <tr>
-                <td>${weerVoor3uur.dt_txt}</td>
-                <td>${(weerVoor3uur.main.temp - 273.15).toFixed(1)}</td>
-                <td>${weerVoor3uur.weather[0].description}</td>
-            </tr>`;
-        });
+    if (!enteredCity) {
+        alert("Please enter a city.");
+        return;
     }
 
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${enteredCity}&appid=${key}`;
+    let response = await fetch(url);
+
+    if (response.ok) {
+        tbody.innerHTML = "";
+        let weatherData = await response.json();
+
+        weatherData.list.forEach(weatherFor3Hours => {
+            tbody.innerHTML += `
+            <tr>
+                <td>${weatherFor3Hours.dt_txt}</td>
+                <td>${(weatherFor3Hours.main.temp - 273.15).toFixed(1)}</td>
+                <td>${weatherFor3Hours.weather[0].description}</td>
+            </tr>`;
+        });
+    } else {
+        alert("City not found. Please try again.");
+    }
 });
